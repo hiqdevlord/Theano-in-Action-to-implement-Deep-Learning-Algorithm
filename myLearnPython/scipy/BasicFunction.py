50e9f0bd5cea7cb1d54d5acd6c657877268c78ee
hiqdevlord <hiqdevlord@gmail.com>
2015-01-19 20:36:14 +0800

    2015-01-19 add scipy basic function & special function

---
+++ myLearnPython/scipy/BasicFunction.py
@@ -0,0 +1,16 @@
+# __author__ = 'cjweffort'
+# -*- coding: utf-8 -*-
+
+"""
+1 Basic Funciton
+"""
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from scipy import linalg, optimize
+
+"""
+1.1 Index Tricks
+"""
